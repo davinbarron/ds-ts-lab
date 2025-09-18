ee9dddb4b3431a4ee5a00aafce885d5b687b29fc
davinbarron <davinbarron667@gmail.com>
2025-09-18 12:42:11 +0100

    Updated functions in 02-functions.ts to use type inferencing.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,7 +1,7 @@
 import { colleagues, friends } from './01-basics'
 import {Friend, Colleague } from './myTypes'
 
-function older(f: Friend) : string {
+function older(f: Friend) {
      f.age += 1
      return `${f.name} is now ${f.age}` 
 }
@@ -10,7 +10,7 @@ console.log(older(friends[0]))
 
 // Using a callback on the older function
 // For each friend in our array we will call the older function
-function allOlder(fs: Friend[]) : string[] {
+function allOlder(fs: Friend[]) {
     const friends = fs.map(f => older(f))
     return friends
 }
@@ -18,7 +18,7 @@ function allOlder(fs: Friend[]) : string[] {
 console.log(allOlder(friends))
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) {
   const result = cs.sort(
     (c1, c2) => c1.contact.extension - c2.contact.extension
   );
@@ -29,7 +29,7 @@ console.log(highestExtension(colleagues.current));
 // Function to add a colleague to an array
 // Setting the extension number to highest extension + 1
 // Will need to callback the highestExtension function here.
-function addColleague(colleagues: Colleague[], name: string, department: string, email: string): void {
+function addColleague(colleagues: Colleague[], name: string, department: string, email: string) {
     const he = highestExtension(colleagues);
     const newExtension = he ? he.contact.extension + 1 : 1;
     const newColleague: Colleague = {
