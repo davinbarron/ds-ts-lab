5a715115dc61abe4331a033b96367fbe9dad1919
davinbarron <davinbarron667@gmail.com>
2025-09-19 16:03:36 +0100

    Optionals parameters.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -49,9 +49,12 @@ console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 function sortColleagues(
   colleagues: Colleague[],
   sorter: (c1: Colleague, c2: Colleague) => number,
-  max : number
+  max? : number
 ): EmailContact[] {
-  const end = max < 2 ? 1 : max
+  let end = colleagues.length;
+  if (max !== undefined) {
+     end = max < 2 ? 1 : max
+  }
   const sorted = colleagues.sort(sorter);
   const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
   return fullResult.slice(0,end)
@@ -59,6 +62,7 @@ function sortColleagues(
 // Test invocations
 console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length)));
 
 // Function to find friends.
 // Searches an array of friends for those that satisfy a criterion.
